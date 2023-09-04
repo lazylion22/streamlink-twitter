8077032b937c4b635f245c80529a43bd7ac023bd
lazylion22 <88854657+lazylion22@users.noreply.github.com>
2023-09-04 20:31:15 +0000

    1

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# streamlink-twitter
-Twitter plugin for Streamlink
+# A Twitter plugin for Streamlink
+
+**To use this plugin :**  
+
+**1.**  create the following folder:
+
+* **Windows:**
+
+    `C:\Users\%username%\AppData\Roaming\streamlink\plugins`
+
+* **Linux:**
+
+    `~/.local/share/streamlink/plugins`
+
+* **Mac:**
+
+    `${HOME}/Library/Application Support/streamlink/plugins`
+        
+**2.** copy `twitter.py` 
+
+**3.** run streamlink:
+
+`streamlink https://twitter.com/i/broadcasts/1vOxwMVpAZqGB best`
+
---
+++ twitter.py
@@ -0,0 +1,39 @@
+from streamlink.plugin import Plugin, pluginmatcher
+from streamlink.stream import HLSStream
+import re
+from random import randint
+
+
+@pluginmatcher(re.compile(r"https?://(?:\S+\.)?twitter.com/i/broadcasts/(?P<id>\w+)"))
+class Twitter(Plugin):
+    def _get_streams(self):
+        liveID = self.match.group("id")
+        headers = {
+            "authorization": "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA",
+            "x-guest-token": str(randint(1, 99999999)),
+        }
+        params = {
+            "include_events": "true",
+            "ids": liveID,
+            "broadcastVersionMap": "",
+        }
+        res = self.session.http.get(
+            "https://twitter.com/i/api/1.1/broadcasts/show.json",
+            headers=headers,
+            params=params,
+        )
+        jsonres = self.session.http.json(res)
+        media_key = jsonres["broadcasts"][liveID]["media_key"]
+
+        res = self.session.http.get(
+            f"https://twitter.com/i/api/1.1/live_video_stream/status/{media_key}",
+            headers=headers,
+        )
+        jsonres = self.session.http.json(res)
+        m3u8 = jsonres["source"]["location"]
+
+        # video_url = self.session.http.json(res)["data"]["video_url"]
+        return HLSStream.parse_variant_playlist(self.session, m3u8)
+
+
+__plugin__ = Twitter
